da4f7a1550612cc759f330d5b90b43ebeab66a11
olegdatskiv <olegdatskiv12@gmail.com>
2019-03-20 18:41:24 +0200

    add MVC configuration and logger

--- TicketsBooking/Startup.cs
+++ TicketsBooking/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace TicketsBooking
 {
@@ -15,18 +16,27 @@ namespace TicketsBooking
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            loggerFactory.AddConsole();
+
+            app.UseAuthentication();
+
             app.Run(async (context) =>
             {
+                var logger = loggerFactory.CreateLogger("RequestInfoLogger");
+
+                logger.LogInformation("Processing request {0}", context.Request.Path);
+
                 await context.Response.WriteAsync("Hello World!");
             });
         }
