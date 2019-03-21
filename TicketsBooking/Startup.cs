04beb2a63e2f2cd9b1c7b88f460c3a8c2480a02b
olegdatskiv <olegdatskiv12@gmail.com>
2019-03-21 17:14:21 +0200

    add UseStaticFile method

--- TicketsBooking/Startup.cs
+++ TicketsBooking/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Routing;
 
 namespace TicketsBooking
 {
@@ -17,6 +18,7 @@ namespace TicketsBooking
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddRouting();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,6 +32,8 @@ namespace TicketsBooking
             loggerFactory.AddConsole();
 
             app.UseAuthentication();
+
+            app.UseStaticFiles();
 
             app.Run(async (context) =>
             {
