59b9403a3abcea4ba6069f21de6fe1bd3b9fa1ab
olegdatskiv <olegdatskiv12@gmail.com>
2019-03-23 19:09:19 +0200

    edit class Startup

--- TicketsBooking/Startup.cs
+++ TicketsBooking/Startup.cs
@@ -44,6 +44,14 @@ namespace TicketsBooking
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                                                        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
