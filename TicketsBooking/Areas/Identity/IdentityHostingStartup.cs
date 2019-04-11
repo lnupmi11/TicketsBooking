d4332407ce79b6e5f23daa24c35cbf140b24638d
olegdatskiv <olegdatskiv12@gmail.com>
2019-04-11 23:18:38 +0300

    add Areas Identity

---
+++ TicketsBooking/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TicketsBooking.Models;
+
+[assembly: HostingStartup(typeof(TicketsBooking.Areas.Identity.IdentityHostingStartup))]
+namespace TicketsBooking.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<TicketsBookingContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("TicketsBookingContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<TicketsBookingContext>();
+            });
+        }
+    }
+}
