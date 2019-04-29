3d6800c12334b2dccb0f56aa5b865c1a033af5f3
olegdatskiv <olegdatskiv12@gmail.com>
2019-04-29 18:26:58 +0300

    add BLL and fixed packeges confflict

--- TicketsBooking.BLL/Services/TicketService.cs
+++ TicketsBooking.BLL/Services/TicketService.cs
@@ -10,42 +10,42 @@ using System.Linq;
 
 namespace TicketsBooking.BLL.Services
 {
-    public class TicketService : IServiceTicket
-    {
-        public void Create(Ticket ticket)
-        {
-            TicketRepository ticketRepository = new TicketRepository();
-            ticketRepository.Create(ticket);
-        }
+    //public class TicketService : IServiceTicket
+    //{
+    //    public void Create(Ticket ticket)
+    //    {
+    //        TicketRepository ticketRepository = new TicketRepository();
+    //        ticketRepository.Create(ticket);
+    //    }
 
-        public void Delete(int id)
-        {
-            using (TicketsBookingContext db = new TicketsBookingContext())
-            {
-                TicketRepository ticketRepository = new TicketRepository();
-                var ticket = db.Tickets.Where(t => t.Id == id).First();
-                ticketRepository.Delete(ticket);
-            }
+    //    public void Delete(int id)
+    //    {
+    //        using (TicketsBookingContext db = new TicketsBookingContext())
+    //        {
+    //            TicketRepository ticketRepository = new TicketRepository();
+    //            var ticket = db.Tickets.Where(t => t.Id == id).First();
+    //            ticketRepository.Delete(ticket);
+    //        }
 
-        }
+    //    }
 
-        public Ticket Get(int id)
-        {
-            TicketRepository ticketRepository = new TicketRepository();
-            return ticketRepository.Get(id.ToString());
+    //    public Ticket Get(int id)
+    //    {
+    //        TicketRepository ticketRepository = new TicketRepository();
+    //        return ticketRepository.Get(id.ToString());
             
-        }
+    //    }
 
-        public IEnumerable<Ticket> GetAll()
-        {
-            TicketRepository ticketRepository = new TicketRepository();
-            return ticketRepository.GetAll();
-        }
+    //    public IEnumerable<Ticket> GetAll()
+    //    {
+    //        TicketRepository ticketRepository = new TicketRepository();
+    //        return ticketRepository.GetAll();
+    //    }
 
-        public void Update(Ticket ticket)
-        {
-            TicketRepository ticketRepository = new TicketRepository();
-            ticketRepository.Update(ticket);
-        }
-    }
+    //    public void Update(Ticket ticket)
+    //    {
+    //        TicketRepository ticketRepository = new TicketRepository();
+    //        ticketRepository.Update(ticket);
+    //    }
+    //}
 }
--- TicketsBooking.BLL/Services/UserService.cs
+++ TicketsBooking.BLL/Services/UserService.cs
@@ -8,51 +8,51 @@ using TicketsBooking.DAL.EntityFramework;
 
 namespace TicketsBooking.BLL.Services
 {
-    public class UserService : IServiceUser
-    {
-        public void ChangeFirstname(User user, string firstname)
-        {
-            using (TicketsBookingContext db = new TicketsBookingContext())
-            {
-                user.FirstName = firstname;
-                db.Update(user);
-            }
-        }
+    //public class UserService : IServiceUser
+    //{
+    //    public void ChangeFirstname(User user, string firstname)
+    //    {
+    //        using (TicketsBookingContext db = new TicketsBookingContext())
+    //        {
+    //            user.FirstName = firstname;
+    //            db.Update(user);
+    //        }
+    //    }
 
-        public void ChangeLastname(User user, string lastname)
-        {
-            using (TicketsBookingContext db = new TicketsBookingContext())
-            {
-                user.LastName = lastname;
-                db.Update(user);
-            }
-        }
+    //    public void ChangeLastname(User user, string lastname)
+    //    {
+    //        using (TicketsBookingContext db = new TicketsBookingContext())
+    //        {
+    //            user.LastName = lastname;
+    //            db.Update(user);
+    //        }
+    //    }
 
-        public void ChangeUsername(User user, string name)
-        {
-            using (TicketsBookingContext db = new TicketsBookingContext())
-            {
-                user.UserName = name;
-                db.Update(user);
-            }
-        }
+    //    public void ChangeUsername(User user, string name)
+    //    {
+    //        using (TicketsBookingContext db = new TicketsBookingContext())
+    //        {
+    //            user.UserName = name;
+    //            db.Update(user);
+    //        }
+    //    }
 
-        public void Delete(User user)
-        {
-            UserRepository userRepository = new UserRepository();
-            userRepository.Delete(user.Id.ToString());
-        }
+    //    public void Delete(User user)
+    //    {
+    //        UserRepository userRepository = new UserRepository();
+    //        userRepository.Delete(user.Id.ToString());
+    //    }
 
-        public IEnumerable<User> GetAll()
-        {
-            UserRepository userRepository = new UserRepository();
-            return userRepository.GetAll();
-        }
+    //    public IEnumerable<User> GetAll()
+    //    {
+    //        UserRepository userRepository = new UserRepository();
+    //        return userRepository.GetAll();
+    //    }
 
-        public User GetUser(string id)
-        {
-            UserRepository userRepository = new UserRepository();
-            return userRepository.Get(id);
-        }
-    }
+    //    public User GetUser(string id)
+    //    {
+    //        UserRepository userRepository = new UserRepository();
+    //        return userRepository.Get(id);
+    //    }
+    //}
 }
