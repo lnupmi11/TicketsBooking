a14867c130b59cccedd78ec5445283ac8eb86839
olegdatskiv <olegdatskiv12@gmail.com>
2019-05-01 18:36:07 +0300

    add order servises

---
+++ TicketsBooking.BLL/Services/OrderService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TicketsBooking.BLL.Interfaces;
+using TicketsBooking.DAL.Interfaces;
+using TicketsBooking.DTO.Cart;
+
+namespace TicketsBooking.BLL.Services
+{
+    public class OrderService : IOrderService
+    {
+        private IUnitOfWork _unitOfWork;
+        public OrderService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void AddItemToBasket(string userName, string itemId)
+        {
+            var item = _unitOfWork.TicketRepository.Get(itemId);
+            if (item != null)
+            {
+               
+                _unitOfWork.SaveChanges();
+            }
+        }
+
+        public void DeleteItemFromBasket(string userName, string itemId)
+        {
+            var item = _unitOfWork.TicketRepository.Get(itemId);
+            if (item != null)
+            {
+                
+                
+            }
+        }
+
+        public IEnumerable<CartItemDTO> GetAllUserBasketItems(string userName)
+        {
+            return null;
+        }
+    }
+}
---
+++ TicketsBooking.DTO/Cart/CartItemDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketsBooking.DTO.Cart
+{
+    public class CartItemDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Count { get; set; }
+    }
+}
