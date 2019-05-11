2453d405bb77c42f71ab8feca3f5302fb80b6db3
olegdatskiv <olegdatskiv12@gmail.com>
2019-05-11 17:11:11 +0300

    add usersController

--- TicketsBooking/Controllers/UsersController.cs
+++ TicketsBooking/Controllers/UsersController.cs
@@ -12,88 +12,64 @@ namespace TicketsBooking.Controllers
 {
     public class UsersController : Controller
     {
-        private IServiceUser _userServices;
+        UserManager<User> _userManager;
 
-        public UsersController(IServiceUser userServices)
+        public UsersController(UserManager<User> userManager)
         {
-            _userServices = userServices;
+            _userManager = userManager;
         }
 
-        public IActionResult Index() => View(_userServices.GetAll());
+        public IActionResult Index() => View(_userManager.Users.ToList());
 
-        public IActionResult Create() => View();
+        public async Task<IActionResult> Edit(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            EditUserViewModel model = new EditUserViewModel { Id = user.Id, Email = user.Email, Year = user.Year };
+            return View(model);
+        }
 
-        //[HttpPost]
-        //public async Task<IActionResult> Create(CreateUserViewModel model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        User user = new User { Email = model.Email, UserName = model.Email, Year = model.Year };
-        //        var result = await _userManager.CreateAsync(user, model.Password);
-        //        if (result.Succeeded)
-        //        {
-        //            return RedirectToAction("Index");
-        //        }
-        //        else
-        //        {
-        //            foreach (var error in result.Errors)
-        //            {
-        //                ModelState.AddModelError(string.Empty, error.Description);
-        //            }
-        //        }
-        //    }
-        //    return View(model);
-        //}
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    user.Email = model.Email;
+                    user.UserName = model.Email;
+                    user.Year = model.Year;
 
-        //public async Task<IActionResult> Edit(string id)
-        //{
-        //    User user = await _userManager.FindByIdAsync(id);
-        //    if (user == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    EditUserViewModel model = new EditUserViewModel { Id = user.Id, Email = user.Email, Year = user.Year };
-        //    return View(model);
-        //}
+                    var result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+            }
+            return View(model);
+        }
 
-        //[HttpPost]
-        //public async Task<IActionResult> Edit(EditUserViewModel model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        User user = await _userManager.FindByIdAsync(model.Id);
-        //        if (user != null)
-        //        {
-        //            user.Email = model.Email;
-        //            user.UserName = model.Email;
-        //            user.Year = model.Year;
-
-        //            var result = await _userManager.UpdateAsync(user);
-        //            if (result.Succeeded)
-        //            {
-        //                return RedirectToAction("Index");
-        //            }
-        //            else
-        //            {
-        //                foreach (var error in result.Errors)
-        //                {
-        //                    ModelState.AddModelError(string.Empty, error.Description);
-        //                }
-        //            }
-        //        }
-        //    }
-        //    return View(model);
-        //}
-
-        //[HttpPost]
-        //public async Task<ActionResult> Delete(string id)
-        //{
-        //    User user = await _userManager.FindByIdAsync(id);
-        //    if (user != null)
-        //    {
-        //        IdentityResult result = await _userManager.DeleteAsync(user);
-        //    }
-        //    return RedirectToAction("Index");
-        //}
+        [HttpPost]
+        public async Task<ActionResult> Delete(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await _userManager.DeleteAsync(user);
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
