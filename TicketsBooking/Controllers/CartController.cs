184b66bb4622ad8fcdfae2c9bcced9a23233c24d
olegdatskiv <olegdatskiv12@gmail.com>
2019-05-11 16:53:42 +0300

    add buy ticket method

--- TicketsBooking/Controllers/CartController.cs
+++ TicketsBooking/Controllers/CartController.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
-using iTextSharp.text;
-using iTextSharp.text.pdf;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TicketsBooking.BLL.Interfaces;
 using TicketsBooking.DAL.Interfaces;
 using TicketsBooking.DTO.Ticket;
 using TicketsBooking.Models;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+using TicketsBooking.Utility;
+using Microsoft.EntityFrameworkCore;
 
 namespace TicketsBooking.Controllers
 {
@@ -19,12 +20,15 @@ namespace TicketsBooking.Controllers
         IOrderService _orderService;
         IServiceTicket _serviceTicket;
         private IUnitOfWork _unitOfWork;
+        private IConverter _converter;
 
-        public CartController(IUnitOfWork unitOfWork, IOrderService orderService, IServiceTicket serviceTicket) : base()
+        public CartController(IUnitOfWork unitOfWork, IOrderService orderService, 
+            IServiceTicket serviceTicket, IConverter converter) : base()
         {
             _orderService = orderService;
             _serviceTicket = serviceTicket;
             _unitOfWork = unitOfWork;
+            _converter = converter;
         }
 
         [Authorize]
@@ -65,35 +69,53 @@ namespace TicketsBooking.Controllers
         [HttpGet]
         public IActionResult Buy(int id)
         {
-            //var globalSettings = new GlobalSettings
-            //{
-            //    ColorMode = ColorMode.Color,
-            //    Orientation = Orientation.Portrait,
-            //    PaperSize = PaperKind.A4,
-            //    Margins = new MarginSettings { Top = 10 },
-            //    DocumentTitle = "PDF Report",
-            //    Out = @"D:\PDFCreator\Employee_Report.pdf"
-            //};
+            var ticket = _unitOfWork.TicketRepository.Get(id.ToString());
+            var flight = _unitOfWork.FlightRepository.GetAll().Where(t => t.Id == ticket.FlightId).FirstOrDefault();
 
-            //var objectSettings = new ObjectSettings
-            //{
-            //    PagesCount = true,
-            //    HtmlContent = TemplateGenerator.GetHTMLString(),
-            //    WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css") },
-            //    HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
-            //    FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
-            //};
+            var viewTicket = new TicketViewModel()
+            {
+                Id = ticket.Id,
+                Price = ticket.Price,
+                FlightArrivingDate = flight.FlightArrivingDate,
+                FlightDepartmentDate = flight.FlightDepartmentDate,
+                LocationFrom = flight.LocationFrom,
+                LocationTo = flight.LocationTo
+            };
 
-            //var pdf = new HtmlToPdfDocument()
-            //{
-            //    GlobalSettings = globalSettings,
-            //    Objects = { objectSettings }
-            //};
+            var listTicket = new List<TicketViewModel>();
+
+            listTicket.Add(viewTicket);
+
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 10 },
+                DocumentTitle = "PDF Report",
+                Out = @"C:\Ticket.pdf"
+            };
+
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                HtmlContent = TemplateGenerator.GetHTMLString(listTicket),
+                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css") },
+                HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
+                FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
+            };
+
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
 
             //_converter.Convert(pdf);
 
-            return Ok("Successfully created PDF document.");
+            _unitOfWork.TicketRepository.Delete(ticket.Id.ToString());
 
+            return RedirectToAction("Index", "Cart");
         }
 
     }
