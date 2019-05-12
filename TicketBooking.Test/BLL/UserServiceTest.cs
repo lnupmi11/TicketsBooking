284a7bfdc598fcc8de9415dfc09f7705b449d45f
dana.hrechyn <dana.hrechyn@gmail.com>
2019-05-12 10:49:45 +0300

    test  file for user

---
+++ TicketBooking.Test/BLL/UserServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using TicketsBooking.BLL.Services;
+using TicketsBooking.DAL;
+using TicketsBooking.DAL.Interfaces;
+using TicketsBooking.DAL.Entities;
+using TicketsBooking.DAL.EntityFramework;
+using TicketsBooking.DAL.UnitOfWork;
+using Xunit;
+
+namespace TicketsBooking.Test.BLL
+{
+    public class UserServiceTest
+    {
+        private UserService service;
+        private IEnumerable<User> users;
+
+        [Fact]
+        public void GetAllUsersTest()
+        {
+            users = service.GetAll();
+           
+        }
+
+    }
+}
