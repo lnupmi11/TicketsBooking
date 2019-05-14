78b15b6cfc19b4bead169ec746f75b9c56fc9ee8
dana.hrechyn <dana.hrechyn@gmail.com>
2019-05-14 19:16:59 +0300

    View all tickets for admin

---
+++ TicketsBooking.BLL/Interfaces/IServiceAdmin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TicketsBooking.DAL.Entities;
+
+namespace TicketsBooking.BLL.Interfaces
+{
+    public interface IServiceAdmin
+    {
+        void BanUser(int id);
+
+        void UnbanUser(int id);
+    }
+}
---
+++ TicketsBooking/Controllers/AdminController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TicketsBooking.BLL.Interfaces;
+using TicketsBooking.DAL.Interfaces;
+using TicketsBooking.DTO.Ticket;
+using TicketsBooking.Models;
+
+namespace TicketsBooking.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IServiceTicket _ticketService;
+        private IOrderService _orderService;
+        private IServiceFlight _flightService;
+        private IServiceUser _serviceUser;
+        private IUnitOfWork _unitOfWork;
+    
+        public AdminController(IUnitOfWork unitOfWork, IServiceTicket ticketService, IOrderService orderService, IServiceFlight serviceFlight, IServiceUser serviceUser) : base()
+        {
+            _ticketService = ticketService;
+            _orderService = orderService;
+            _flightService = serviceFlight;
+            _serviceUser = serviceUser;
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult AddTicket()
+        {
+            return View();
+        }
+
+        public IActionResult GetAllTickets()
+        {
+            var flights = _flightService.GetAll();
+
+            var tickets = new List<TicketViewModel>();
+            foreach (var iteam in flights)
+            {
+                var itemTickets = _ticketService.GetAll().Where(x => x.FlightID == iteam.Id).ToList();
+                foreach (var ticket in itemTickets)
+                {
+                    var viewTicket = new TicketViewModel()
+                    {
+                        Id = ticket.Id,
+                        Price = ticket.Price,
+                        FlightArrivingDate = iteam.FlightArrivingDate,
+                        FlightDepartmentDate = iteam.FlightDepartmentDate,
+                        LocationFrom = iteam.LocationFrom,
+                        LocationTo = iteam.LocationTo
+                    };
+                    tickets.Add(viewTicket);
+                }
+            }
+            if (tickets.Count != 0)
+            {
+                return View(tickets);
+            }
+            return View();
+        }
+
+    }
+}
