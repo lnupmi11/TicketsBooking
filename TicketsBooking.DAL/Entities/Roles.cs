ce01d95f7426da81ad42fe0e6240b954cefc70e1
dana.hrechyn <dana.hrechyn@gmail.com>
2019-05-14 17:13:36 +0300

    Added role for admin

---
+++ TicketsBooking.DAL/Entities/Roles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace TicketsBooking.DAL.Entities
+{
+    public enum Roles
+    {
+        [Description("Admin")]
+        Admin,
+        [Description("User")]
+        User,
+    }
+}
