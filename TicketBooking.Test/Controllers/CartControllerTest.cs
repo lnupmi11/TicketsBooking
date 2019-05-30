c66ead0dd34b990054cb2d6ddf8cbf252c709446
dana.hrechyn <dana.hrechyn@gmail.com>
2019-05-30 17:24:59 +0300

    added files for new tests

--- TicketBooking.Test/Controllers/CartControllerTest.cs
+++ TicketBooking.Test/Controllers/CartControllerTest.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace TicketsBooking.Test.BLL
+namespace TicketsBooking.Test.Controllers
 {
-    class OrderServiceTest
+    class CartControllerTests
     {
     }
 }
---
+++ TicketBooking.Test/Controllers/HomeControllerTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketsBooking.Test.Controllers
+{
+    class HomeControllerTest
+    {
+    }
+}
